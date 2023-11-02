906eb53253a77e6eaecf7d6f823122dfc9641d3a
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-11-02 10:48:23 +0800

    Added the Axis angle display panel.

--- src/Controllers/BraccioController.py
+++ src/Controllers/BraccioController.py
@@ -1,15 +1,98 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        uiRun.py
+#
+# Purpose:     This module is used as a sample to create the main wx frame.
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2019/01/10
+# Copyright:   YC @ Singtel Cyber Security Research & Development Laboratory
+# License:     YC
+#-----------------------------------------------------------------------------
+import os
+import sys
 import time
-import serialCom
+import wx
+import uiGobal as gv
+import BraccioControllerPnl as pl
+PERIODIC = 500      # update in every 500ms
 
-connector = serialCom.serialCom(None, serialPort='COM3',baudRate=9600)
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+class UIFrame(wx.Frame):
+    """ Main UI frame window."""
+    def __init__(self, parent, id, title):
+        """ Init the UI and parameters """
+        wx.Frame.__init__(self, parent, id, title, size=(1150, 560))
+        # No boader frame:
+        #wx.Frame.__init__(self, parent, id, title, style=wx.MINIMIZE_BOX | wx.STAY_ON_TOP)
+        self.SetBackgroundColour(wx.Colour(200, 210, 200))
+        #self.SetTransparent(gv.gTranspPct*255//100)
+        #self.SetIcon(wx.Icon(gv.ICO_PATH))
+        # Build UI sizer
+        self.SetSizer(self._buidUISizer())
+        # Set the periodic call back
+        self.lastPeriodicTime = time.time()
+        self.timer = wx.Timer(self)
+        self.updateLock = False
+        self.Bind(wx.EVT_TIMER, self.periodic)
+        self.timer.Start(PERIODIC)  # every 500 ms
 
-if connector.connected:
-    while True:
-        print("Connected to the Braccio arm")
-        print("Input the cmd :")
-        cmd = str(input())
-        connector.write(cmd.encode('utf-8'))
-        time.sleep(3)
-        print("Got feed back:")
-        msg = connector.readline()
-        print(msg)
+#--UIFrame---------------------------------------------------------------------
+    def _buidUISizer(self):
+        """ Build the main UI Sizer. """
+        flagsR = wx.LEFT
+        mSizer = wx.BoxSizer(wx.VERTICAL)
+        mSizer.AddSpacer(5)
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        self.gripSizer = self._buildGripSizer()
+        hbox1.Add(self.gripSizer, flag=flagsR, border=2)
+        mSizer.AddSpacer(5)
+
+        mSizer.Add(hbox1, flag=flagsR, border=2)
+        return mSizer
+
+    def _buildGripSizer(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        font = wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+        label = wx.StaticText(self, label="Gripper Ctrl")
+        label.SetFont(font)
+        sizer.Add(label, flag=wx.LEFT, border=2)
+        sizer.AddSpacer(10)
+        self.gripDis = pl.angleDisplayPanel(self, "Gripper", limitRange=(70, 230))
+        sizer.Add(self.gripDis, flag=wx.LEFT, border=2)
+        sizer.AddSpacer(5)
+        sizer.Add(wx.StaticText(self, label=" Motor Axis Angle Control:".ljust(15)), flag=wx.LEFT)
+        sizer.AddSpacer(5)
+        self.gripperCtrl = wx.Slider(self, value = int(159), minValue = 158, maxValue = 230, size=(270, 30),
+        style = wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.gripperCtrl.Bind(wx.EVT_SCROLL_THUMBRELEASE, self.onGripAdj)
+        sizer.Add(self.gripperCtrl, flag=wx.CENTRE)
+        sizer.AddSpacer(5)
+        return sizer
+    
+    def onGripAdj(self, event):
+        val = self.gripperCtrl.GetValue()
+        print(val)
+
+#--UIFrame---------------------------------------------------------------------
+    def periodic(self, event):
+        """ Call back every periodic time."""
+        now = time.time()
+        if (not self.updateLock) and now - self.lastPeriodicTime >= gv.gUpdateRate:
+            print("main frame update at %s" % str(now))
+            self.lastPeriodicTime = now
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+class MyApp(wx.App):
+    def OnInit(self):
+        gv.iMainFrame = UIFrame(None, -1, gv.APP_NAME)
+        gv.iMainFrame.Show(True)
+        return True
+
+#-----------------------------------------------------------------------------
+if __name__ == '__main__':
+    app = MyApp(0)
+    app.MainLoop()
---
+++ src/Controllers/BraccioControllerPnl.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        uiPanel.py
+#
+# Purpose:     This module is used to create different function panels.
+# Author:      Yuancheng Liu
+#
+# Created:     2020/01/10
+# Copyright:   YC @ Singtel Cyber Security Research & Development Laboratory
+# License:     YC
+#-----------------------------------------------------------------------------
+import wx
+import math
+
+from datetime import datetime
+import uiGobal as gv
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+class angleDisplayPanel(wx.Panel):
+    """ Panel to display image. """
+
+    def __init__(self, parent, title, limitRange = (75, 220), panelSize=(300, 300)):
+        wx.Panel.__init__(self, parent, size=panelSize)
+        self.SetBackgroundColour(wx.Colour(200, 200, 200))
+        self.panelSize = panelSize
+        self.title = title
+        self.limitRange = limitRange
+        self.angle1 = 90.1
+        self.angle2 = 120.4
+        self.bmp = wx.Bitmap(gv.BGIMG_PATH, wx.BITMAP_TYPE_ANY)
+        self.Bind(wx.EVT_PAINT, self.onPaint)
+        self.SetDoubleBuffered(True)
+
+#--PanelImge--------------------------------------------------------------------
+    def onPaint(self, evt):
+        """ Draw the map on the panel."""
+        dc = wx.PaintDC(self)
+        dc.DrawBitmap(self.bmp, 0, 0)
+        
+        dc.SetFont(wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+        dc.SetTextForeground(wx.Colour('GREEN'))
+        dc.DrawText(self.title, 5, 5)
+        # draw the range detail.
+        dc.SetPen(wx.Pen('Green'))
+        dc.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL))
+        x1, y1 = 150-math.sin(math.radians(self.limitRange[0]))*130, 150+math.cos(math.radians(self.limitRange[0]))*130
+        dc.DrawLine(150,150, x1, y1)
+        dc.DrawText(str(self.limitRange[0]), x1+5, y1-5)
+
+        x2, y2 = 150-math.sin(math.radians(self.limitRange[1]))*130, 150+math.cos(math.radians(self.limitRange[1]))*130
+        dc.DrawLine(150,150, x2, y2)
+        dc.DrawText(str(self.limitRange[1]), x2+5, y2-5)
+        dc.SetTextForeground(wx.Colour('YELLOW'))
+        dc.SetPen(wx.Pen(wx.Colour('YELLOW'), width=3, style=wx.PENSTYLE_SHORT_DASH))
+        if not self.angle1 is None:
+            dc.DrawText("Output-1 Axis:%s" %str(self.angle1), 160, 180)
+            x3, y4 = 150-math.sin(math.radians(self.angle1))*130, 150+math.cos(math.radians(self.angle1))*130
+            dc.DrawLine(150,150, x3, y4)
+
+        if not self.angle2 is None:
+            dc.DrawText("Output-2 Axis:%s" %str(self.angle2), 160, 200)
+            x5, y6 = 150-math.sin(math.radians(self.angle2))*130, 150+math.cos(math.radians(self.angle2))*130
+            dc.DrawLine(150,150, x5, y6)
+
+    #-----------------------------------------------------------------------------
+    def updateAngle(self, angle1=None, angle2=None):
+        self.angle1 = angle1
+        self.angle2 = angle2
+
+    #-----------------------------------------------------------------------------
+    def updateDisplay(self, updateFlag=None):
+        """ Set/Update the display: if called as updateDisplay() the function will 
+            update the panel, if called as updateDisplay(updateFlag=?) the function
+            will set the self update flag.
+        """
+        self.Refresh(False)
+        self.Update()
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+def main():
+    """ Main function used for local test debug panel. """
+
+    print('Test Case start: type in the panel you want to check:')
+    print('0 - PanelImge')
+    print('1 - PanelCtrl')
+    #pyin = str(input()).rstrip('\n')
+    #testPanelIdx = int(pyin)
+    testPanelIdx = 1    # change this parameter for you to test.
+    print("[%s]" %str(testPanelIdx))
+    app = wx.App()
+    mainFrame = wx.Frame(gv.iMainFrame, -1, 'Debug Panel',
+                         pos=(300, 300), size=(640, 480), style=wx.DEFAULT_FRAME_STYLE)
+
+    testPanel = angleDisplayPanel(mainFrame, title="grip")
+    mainFrame.Show()
+    app.MainLoop()
+
+if __name__ == "__main__":
+    main()
+
+
+
