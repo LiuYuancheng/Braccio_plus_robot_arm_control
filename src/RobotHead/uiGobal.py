efa5d3125670462f83ed3d3cd785cd660ac6f29a
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-11-07 18:32:07 +0800

    Add the robot face UI program.

---
+++ src/RobotHead/uiGobal.py
@@ -0,0 +1,41 @@
+#-----------------------------------------------------------------------------
+# Name:        uiGlobal.py
+#
+# Purpose:     This module is used as a local config file to set constants, 
+#              global parameters which will be used in the other modules.
+#              
+# Author:      Yuancheng Liu
+#
+# Created:     2020/01/10
+# Copyright:   YC @ Singtel Cyber Security Research & Development Laboratory
+# License:     YC
+#-----------------------------------------------------------------------------
+import os
+
+print("Current working directory is : %s" % os.getcwd())
+dirpath = os.path.dirname(__file__)
+print("Current source code location : %s" % dirpath)
+APP_NAME = 'Robot YG'
+
+#------<IMAGES PATH>-------------------------------------------------------------
+IMG_FD = 'img'
+ICO_PATH = os.path.join(dirpath, IMG_FD, "geoIcon.ico")
+BGIMG_PATH = os.path.join(dirpath, IMG_FD, "background.png")
+
+SLP_1_IMG = os.path.join(dirpath, IMG_FD, "sleep_1.png")
+SLP_2_IMG = os.path.join(dirpath, IMG_FD, "sleep_2.png")
+SLP_3_IMG = os.path.join(dirpath, IMG_FD, "sleep_3.png")
+
+LOOK_L_IMG = os.path.join(dirpath, IMG_FD, "look_left.png")
+LOOK_R_IMG = os.path.join(dirpath, IMG_FD, "look_right.png")
+
+#-------<GLOBAL VARIABLES (start with "g")>------------------------------------
+# VARIABLES are the built in data type.
+gTranspPct = 70     # Windows transparent percentage.
+gUpdateRate = 1     # main frame update rate 1 sec.
+
+#-------<GLOBAL PARAMTERS>-----------------------------------------------------
+iMainFrame = None   # MainFrame.
+iImagePanel = None  # Image panel.
+iCtrlPanel = None   # control panel
+
